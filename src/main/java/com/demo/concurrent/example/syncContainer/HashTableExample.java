c21581ea469634626dbad30c8b66ec3b8d4d3f81
hobo <1056024860@qq.com>
2019-10-30 19:21:00 +0800

    将map换位HashMap使其线程安全

---
+++ src/main/java/com/demo/concurrent/example/syncContainer/HashTableExample.java
@@ -0,0 +1,59 @@
+package com.demo.concurrent.example.syncContainer;
+
+import com.demo.concurrent.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author hobo
+ * @description 将HashMap变为HashTable，使得线程安全
+ */
+@Slf4j
+@ThreadSafe
+public class HashTableExample {
+
+    // 请求总数
+    // 若线程安全。则长度应为5000
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 50;
+
+    private static Map<Integer,Integer> map = new Hashtable<>();
+
+
+    public static void main(String[] args) throws Exception{
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            int finalI = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add(finalI);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", map.size());
+
+    }
+
+    private static void add(int i) {
+        map.put(i,i);
+    }
+}
