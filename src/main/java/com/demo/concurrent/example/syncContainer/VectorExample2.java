75be98a60f65bf6d0dafba26dda6af087811c577
hobo <1056024860@qq.com>
2019-10-30 19:12:21 +0800

    线程不安全Vector演示

---
+++ src/main/java/com/demo/concurrent/example/syncContainer/VectorExample2.java
@@ -0,0 +1,45 @@
+package com.demo.concurrent.example.syncContainer;
+
+import com.demo.concurrent.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Vector;
+
+/**
+ * @author hobo
+ * @description Vector 线程同步.但并发时可能会出现异常
+ */
+@Slf4j
+@ThreadSafe
+public class VectorExample2 {
+
+    private static Vector<Integer> vector = new Vector<>();
+
+    public static void main(String[] args) {
+        while (true) {
+            for (int i = 0; i < vector.size(); i++) {
+                vector.add(i);
+            }
+
+            Thread thread1 = new Thread() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < vector.size(); i++) {
+                        vector.remove(i);
+                    }
+                }
+            };
+
+            Thread thread2 = new Thread() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        vector.get(i);
+                    }
+                }
+            };
+            thread1.start();
+            thread2.start();
+        }
+    }
+}
