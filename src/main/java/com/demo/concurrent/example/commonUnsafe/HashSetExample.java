514c8dc7a35378683ed11eb294d503e82510f192
hobo <1056024860@qq.com>
2019-10-30 17:08:48 +0800

    验证HashSet线程不安全

---
+++ src/main/java/com/demo/concurrent/example/commonUnsafe/HashSetExample.java
@@ -0,0 +1,57 @@
+package com.demo.concurrent.example.commonUnsafe;
+
+import com.demo.concurrent.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author hobo
+ * @description
+ */
+@Slf4j
+@NotThreadSafe
+public class HashSetExample {
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    private static Set<Integer> set = new HashSet<>();
+
+
+    public static void main(String[] args) throws Exception{
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            int finalI = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add(finalI);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", set.size());
+
+    }
+
+    private static void add(int i) {
+        set.add(i);
+    }
+}
