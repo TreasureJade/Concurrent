887d726e8bcb1cec87ef07f37d2276872a9ef2b1
hobo <1056024860@qq.com>
2019-10-31 12:50:10 +0800

    验证ConcurrentSkipListSet 写操作为线程安全

---
+++ src/main/java/com/demo/concurrent/example/concurrent/ConcurrentSkipListSetExample.java
@@ -0,0 +1,55 @@
+package com.demo.concurrent.example.concurrent;
+
+import com.demo.concurrent.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+import java.util.concurrent.*;
+
+/**
+ * @author hobo
+ * @description 验证ConcurrentSkipListSet 写操作为线程安全
+ */
+@Slf4j
+@ThreadSafe
+public class ConcurrentSkipListSetExample {
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    private static Set<Integer> set = new ConcurrentSkipListSet<>();
+
+
+    public static void main(String[] args) throws Exception{
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            int finalI = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add(finalI);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", set.size());
+
+    }
+
+
+    private static void add(int i) {
+        // add操作加锁
+        set.add(i);
+    }
+}
